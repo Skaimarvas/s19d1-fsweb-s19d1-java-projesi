5f2e58a718c58f27d6977b23fe43f4958e337bc6
skaimarvas <skaimarvas@gmail.com>
2024-02-20 19:27:47 +0300

    methods were added

--- src/main/java/com/example/s19d1/service/MovieService.java
+++ src/main/java/com/example/s19d1/service/MovieService.java
@@ -1,4 +1,18 @@
 package com.example.s19d1.service;
 
+import com.example.s19d1.entity.Movie;
+
+import java.util.List;
+
 public interface MovieService {
+    List<Movie> findAll();
+
+    Movie findById(int id);
+
+    Movie save(Movie movie);
+
+    Movie delete(int id);
+
+    Movie update(int id, Movie movie);
+
 }
