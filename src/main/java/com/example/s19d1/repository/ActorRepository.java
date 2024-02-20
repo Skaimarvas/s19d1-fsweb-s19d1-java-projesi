5034dd0f2ef0bcb2d184bf85cb252c247ce49beb
skaimarvas <skaimarvas@gmail.com>
2024-02-20 19:25:17 +0300

    repositories were created

---
+++ src/main/java/com/example/s19d1/repository/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.example.s19d1.repository;
+
+import com.example.s19d1.entity.Actor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActorRepository extends JpaRepository<Actor,Long> {
+}
